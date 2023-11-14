8bab235150c44d71f8354abe5084fb51ddd49f2f
kchranger <kchranger@gmail.com>
2023-11-14 14:10:45 +0900

    test(order) : check, is order successful

--- src/test/java/christmas/ChristmasEventTest.java
+++ src/test/java/christmas/ChristmasEventTest.java
@@ -19,8 +19,8 @@ public class ChristmasEventTest {
     private LocalDate visitingDay;
 
     @BeforeEach
-    void setVisitingDay(){
-        visitingDay = LocalDate.of(EVENT_YEAR,EVENT_MONTH,14); // Random day
+    void setVisitingDay() {
+        visitingDay = LocalDate.of(EVENT_YEAR, EVENT_MONTH, 14); // Random day
     }
 
     @DisplayName("입력한 요일과 날짜를 올바르게 반환하는지 확인")
@@ -30,6 +30,23 @@ public class ChristmasEventTest {
         assertThat(visitingDay.getMonth()).isEqualTo(Month.DECEMBER);
         assertThat(visitingDay.getDayOfMonth()).isEqualTo(14);
         assertThat(visitingDay.getDayOfWeek()).isEqualTo(DayOfWeek.THURSDAY);
+    }
+
+    @DisplayName("주문이 올바르게 입력되었는지 확인")
+    @Test
+    void checkOrderMenu(){
+        Order order = new Order(visitingDay);
+        order.addFood(FoodMenu.BBQ_RIB,1);
+        order.addFood(FoodMenu.MUSHROOM_SOUP,3);
+        order.addFood(FoodMenu.ICE_CREAM,2);
+
+        assertThat(order.getNumberOfMenu(FoodMenu.BBQ_RIB)).isEqualTo(1);
+        assertThat(order.getNumberOfMenu(FoodMenu.BBQ_RIB)).isNotEqualTo(0);
+        assertThat(order.getNumberOfMenu(FoodMenu.BBQ_RIB)).isNotEqualTo(2);
+
+        assertThat(order.getNumberOfMenu(FoodMenu.MUSHROOM_SOUP)).isEqualTo(3);
+        assertThat(order.getNumberOfMenu(FoodMenu.ICE_CREAM)).isEqualTo(2);
+        assertThat(order.getNumberOfMenu(FoodMenu.RED_WINE)).isEqualTo(0);
     }
 
 }
