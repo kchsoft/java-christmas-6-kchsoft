4517ca513474b1235e9ba5ad0c486cbc073fa69d
kchranger <kchranger@gmail.com>
2023-11-15 21:30:38 +0900

    test(exception) : check exception case
    - check all case in README.md

--- src/test/java/christmas/ApplicationTest.java
+++ src/test/java/christmas/ApplicationTest.java
@@ -1,9 +1,14 @@
 package christmas;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static christmas.Constant.ErrorMsgConstant.ERROR_EXCEED_MAX_ORDER_NUMBER;
+import static christmas.Constant.ErrorMsgConstant.ERROR_NOT_VALID_DAY;
+import static christmas.Constant.ErrorMsgConstant.ERROR_NOT_VALID_ORDER;
+import static christmas.Constant.ErrorMsgConstant.ERROR_ONLY_BEVERAGE_ORDER_NOT_ALLOW;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
@@ -46,6 +51,78 @@ class ApplicationTest extends NsTest {
         assertSimpleTest(() -> {
             runException("3", "제로콜라-a");
             assertThat(output()).contains("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        });
+    }
+
+    @DisplayName("음료만 주문하면 예외 발생")
+    @Test
+    public void createOnlyBeverageOrder(){
+        assertSimpleTest(() -> {
+            runException("3", "제로콜라-3,레드와인-4");
+            assertThat(output()).contains(ERROR_ONLY_BEVERAGE_ORDER_NOT_ALLOW);
+        });
+    }
+
+    @DisplayName("주문 개수가 20개가 넘어가면 예외 발생")
+    @Test
+    public void createOverOrder(){
+        assertSimpleTest(() -> {
+            runException("3", "제로콜라-30,해산물파스타-20");
+            assertThat(output()).contains(ERROR_EXCEED_MAX_ORDER_NUMBER);
+        });
+    }
+
+    @DisplayName("공백 입력시 예외 발생")
+    @Test
+    public void createEmptyInput(){
+        assertSimpleTest(() -> {
+            runException("", "제로콜라-3,초코케이크-20");
+            assertThat(output()).contains(ERROR_NOT_VALID_DAY);
+        });
+    }
+
+    @DisplayName("날짜 입력 범위를 벗어나면 예외 발생")
+    @Test
+    public void createOverRangeDay(){
+        assertSimpleTest(() -> {
+            runException("32", "초코케이크-5,레드와인-2");
+            assertThat(output()).contains(ERROR_NOT_VALID_DAY);
+        });
+    }
+
+    @DisplayName("메뉴 입력 형식이 벗어나면 예외 발생")
+    @Test
+    public void createIllegalMenuFormat(){
+        assertSimpleTest(() -> {
+            runException("13", "초코케이크-5 레드와인-2");
+            assertThat(output()).contains(ERROR_NOT_VALID_ORDER);
+        });
+    }
+
+    @DisplayName("중복 메뉴 입력시 예외 발생")
+    @Test
+    public void createDuplicatedMenu(){
+        assertSimpleTest(() -> {
+            runException("3", "제로콜라-1,제로콜라-4");
+            assertThat(output()).contains(ERROR_NOT_VALID_ORDER);
+        });
+    }
+    
+    @DisplayName("없는 메뉴 주문시 예외 발생")
+    @Test
+    public void createNotExistMenu(){
+        assertSimpleTest(() -> {
+            runException("3", "김밥-3");
+            assertThat(output()).contains(ERROR_NOT_VALID_ORDER);
+        });
+    }
+
+    @DisplayName("메뉴 개수가 1 미만이면 예외 발생")
+    @Test
+    public void createUnderMinOrder(){
+        assertSimpleTest(() -> {
+            runException("3", "아이스크림-0");
+            assertThat(output()).contains(ERROR_NOT_VALID_ORDER);
         });
     }
 
