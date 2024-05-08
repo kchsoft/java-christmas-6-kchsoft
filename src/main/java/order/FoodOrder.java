849f53f045e41487d7ed35dc53bd55b8b175ae57
kchranger <kchranger@gmail.com>
2024-05-08 22:22:35 +0900

    feature(input) : validate food order data
    - Integer validate (amount)
    - Food name validate
    - Input format validate
    - If it invalidate, retry input
    - Rename variable

---
+++ src/main/java/order/FoodOrder.java
@@ -0,0 +1,22 @@
+package order;
+
+import menu.Food;
+
+public class FoodOrder {
+    private final Food food;
+    private final Integer amount;
+
+    public FoodOrder(Food food, Integer amount) {
+        this.food = food;
+        this.amount = amount;
+    }
+
+    public Food getFood() {
+        return food;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+}
