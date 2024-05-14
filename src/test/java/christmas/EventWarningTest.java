8b3ee3dd497bcbcc71ea27c8ec93e60b672ccb12
kchranger <kchranger@gmail.com>
2024-05-15 00:50:41 +0900

    test(eventwarning) : create test

---
+++ src/test/java/christmas/EventWarningTest.java
@@ -0,0 +1,93 @@
+package christmas;
+
+import customer.Reservation;
+import customer.VisitingDay;
+import exception.EventApplyException;
+import exception.FoodOrderException;
+import food.Food;
+import order.Order;
+import order.Orders;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class EventWarningTest {
+
+    Reservation reservation;
+    VisitingDay day;
+    Orders orders;
+
+    @BeforeEach
+    void createReservation() {
+        orders = new Orders();
+        day = new VisitingDay(10);
+    }
+
+    @Test
+    @DisplayName("10,000원 이상 주문시 이벤트 적용 가능")
+    void eventApplySuccess() {
+        orders.add(new Order(Food.ICE_CREAM, 5));
+        orders.add(new Order(Food.ZERO_COLA, 5));
+        reservation = new Reservation(day, orders);
+        assertDoesNotThrow(
+                () -> EventWarning.check(reservation));
+    }
+
+    @Test
+    @DisplayName("10,000원 미만 주문시 이벤트 적용 불가")
+    void eventApplyFail() {
+        orders.add(new Order(Food.ICE_CREAM, 1));
+        orders.add(new Order(Food.ZERO_COLA, 1));
+        reservation = new Reservation(day, orders);
+        assertThrows(EventApplyException.class,
+                () -> EventWarning.check(reservation));
+    }
+
+    @Test
+    @DisplayName("음료와 음식 주문시 주문 성공")
+    void orderSuccess() {
+        orders.add(new Order(Food.ICE_CREAM, 5));
+        orders.add(new Order(Food.ZERO_COLA, 5));
+        reservation = new Reservation(day, orders);
+        assertDoesNotThrow(
+                () -> EventWarning.check(reservation));
+    }
+
+    @Test
+    @DisplayName("음료만 주문시 주문 실패")
+    void orderFail() {
+        orders.add(new Order(Food.ZERO_COLA, 5));
+        orders.add(new Order(Food.RED_WINE, 5));
+        reservation = new Reservation(day, orders);
+        assertThrows(FoodOrderException.class,
+                () -> EventWarning.check(reservation));
+    }
+
+    @Test
+    @DisplayName("주문 수량 20개 이하시 주문 성공")
+    void orderAmountSuccess() {
+        orders.add(new Order(Food.ICE_CREAM, 5));
+        orders.add(new Order(Food.ZERO_COLA, 5));
+        orders.add(new Order(Food.T_BONE_STEAK, 5));
+        reservation = new Reservation(day, orders);
+        assertDoesNotThrow(
+                () -> EventWarning.check(reservation));
+
+
+    }
+
+    @Test
+    @DisplayName("주문 수량 20개 초과시 주문 실패")
+    void orderAmountFail() {
+        orders.add(new Order(Food.ICE_CREAM, 10));
+        orders.add(new Order(Food.ZERO_COLA, 10));
+        orders.add(new Order(Food.T_BONE_STEAK, 1));
+        reservation = new Reservation(day, orders);
+        assertThrows(FoodOrderException.class,
+                () -> EventWarning.check(reservation));
+    }
+
+}
