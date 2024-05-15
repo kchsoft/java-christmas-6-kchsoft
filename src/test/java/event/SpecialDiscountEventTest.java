1e57534de77822b46b494748254877232f54104c
kchranger <kchranger@gmail.com>
2024-05-16 03:11:48 +0900

    test(event) : create special discount test

---
+++ src/test/java/event/SpecialDiscountEventTest.java
@@ -0,0 +1,39 @@
+package event;
+
+import customer.Reservation;
+import customer.VisitingDay;
+import eventhistory.EventHistory;
+import food.Food;
+import order.Order;
+import order.Orders;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SpecialDiscountEventTest {
+
+    Event event = new SpecialDiscountEvent();
+    Orders orders = new Orders();
+    VisitingDay day;
+
+    @Test
+    @DisplayName("특별한 날에 음식 주문시 할인 성공")
+    void specialDayDiscountSuccess() {
+        day = new VisitingDay(3);
+        orders.add(new Order(Food.SEAFOOD_PASTA, 2));
+        EventHistory history = event.apply(new Reservation(day, orders));
+        assertThat(history.getBenefit()).isEqualTo(1000);
+    }
+
+    @Test
+    @DisplayName("일반적인 날에 음식 주문시 할인 실패")
+    void specialDayDiscountFail() {
+        day = new VisitingDay(5);
+        orders.add(new Order(Food.SEAFOOD_PASTA, 2));
+        EventHistory history = event.apply(new Reservation(day, orders));
+        assertThat(history.getBenefit()).isEqualTo(0);
+    }
+
+}
