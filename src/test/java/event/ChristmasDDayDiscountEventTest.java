9a625acd381c6607d70a2147a86160ca90760843
kchranger <kchranger@gmail.com>
2024-05-15 11:39:58 +0900

    test(christmasdday) : create test

---
+++ src/test/java/event/ChristmasDDayDiscountEventTest.java
@@ -0,0 +1,36 @@
+package event;
+
+import customer.Reservation;
+import customer.VisitingDay;
+import eventhistory.EventHistory;
+import food.Food;
+import order.Order;
+import order.Orders;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ChristmasDDayDiscountEventTest {
+
+    ChristmasDDayDiscountEvent event = new ChristmasDDayDiscountEvent();
+    Orders orders = new Orders();
+    VisitingDay day = new VisitingDay(1);
+
+    @Test
+    @DisplayName("1~25일에 음식 주문시 할인 성공")
+    void discountSuccess() {
+        orders.add(new Order(Food.SEAFOOD_PASTA, 5));
+        EventHistory history = event.apply(new Reservation(day, orders));
+        assertThat(history.getBenefit()).isEqualTo(1000);
+    }
+
+    @Test
+    @DisplayName("26일 이후에 음식 주문시 할인 적용 불가")
+    void discountFail() {
+        day = new VisitingDay(27);
+        orders.add(new Order(Food.SEAFOOD_PASTA, 5));
+        EventHistory history = event.apply(new Reservation(day, orders));
+        assertThat(history.getBenefit()).isEqualTo(0);
+    }
+}
